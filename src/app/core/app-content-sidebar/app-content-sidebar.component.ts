fb203f2fee9d08a084575f53980b84abff8e03e7
Bendy Zhang <zb@bndy.net>
2020-09-13 20:10:11 +0800

    autoclose content sidebar if elementsToOpen attribute specified

--- src/app/core/app-content-sidebar/app-content-sidebar.component.ts
+++ src/app/core/app-content-sidebar/app-content-sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, ViewChild, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import { Component, EventEmitter, HostListener, Input, ViewChild, OnChanges, OnInit, Output, SimpleChanges, ElementRef } from '@angular/core';
 import { MatDrawer } from '@angular/material/sidenav';
 import { EventsService } from 'src/app/_services';
 
@@ -10,12 +10,14 @@ import { EventsService } from 'src/app/_services';
 export class AppContentSidebarComponent implements OnInit, OnChanges {
   @Input() title: string;
   @Input() open: boolean;
+  @Input() elementsToOpen: string;
   @Input() progressShown: boolean;
   @Output() closed = new EventEmitter();
   @ViewChild('root') rootElement: MatDrawer;
 
   constructor(
     private events: EventsService,
+    private element: ElementRef,
   ) { }
 
   ngOnInit(): void {
@@ -43,5 +45,36 @@ export class AppContentSidebarComponent implements OnInit, OnChanges {
 
   closeProgress(): void {
     this.progressShown = false;
+  }
+
+  @HostListener('document: click', ['$event'])
+  closeClickOutside(event: Event): void {
+    if (this.elementsToOpen && !this.element.nativeElement.contains(event.target)) {
+      let fromElementOpen = false;
+      const elements = document.querySelectorAll(this.elementsToOpen);
+      for (let index = 0; index < elements.length; index++) {
+        if (!fromElementOpen) {
+          fromElementOpen = this.inElement(event.target as Node, elements.item(index));
+        }
+      }
+      if (!fromElementOpen) {
+        this.close();
+      }
+    }
+  }
+
+  private inElement(source: Node, target: Node): boolean {
+    if (source === target) {
+      return true;
+    }
+
+    for (let index = 0; index < target.childNodes.length; index++) {
+      if (source === target.childNodes.item(index)) {
+        console.debug(target.childNodes.item(index));
+        return true;
+      } else {
+        return this.inElement(source, target.childNodes.item(index));
+      }
+    }
   }
 }
