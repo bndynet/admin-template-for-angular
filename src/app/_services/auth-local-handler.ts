76534491837a6f3362a4326564980f8760ea28f8
Bendy Zhang <zb@bndy.net>
2021-03-20 17:09:53 +0800

    feat: store data for local auth type

--- src/app/_services/auth-local-handler.ts
+++ src/app/_services/auth-local-handler.ts
@@ -5,6 +5,9 @@ import { getLocalUrl } from 'src/utils';
 import { AuthHandler, AuthType, UserInfo } from '../app-types';
 import { TokenInfo } from './auth-oauth-handler';
 
+const KEY_USER = 'app_user';
+const KEY_TOKEN = 'app_token';
+
 @Injectable({
   providedIn: 'root',
 })
@@ -12,7 +15,12 @@ export class AuthLocalHandler implements AuthHandler {
   private tokenInfo: TokenInfo;
   private userInfo: UserInfo;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    if (sessionStorage.getItem(KEY_TOKEN) && sessionStorage.getItem(KEY_USER)) {
+      this.tokenInfo = JSON.parse(sessionStorage.getItem(KEY_TOKEN));
+      this.userInfo = JSON.parse(sessionStorage.getItem(KEY_USER));
+    }
+  }
 
   getAuthType(): AuthType {
     return AuthType.Local;
@@ -46,6 +54,8 @@ export class AuthLocalHandler implements AuthHandler {
           this.tokenInfo = {
             accessToken: password,
           };
+          sessionStorage.setItem(KEY_TOKEN, JSON.stringify(this.tokenInfo));
+          sessionStorage.setItem(KEY_USER, JSON.stringify(this.userInfo));
           return this.userInfo;
         })
       )
