541c031719b8d9b254e6267bd6f25f30a49f8394
Bendy Zhang <zb@bndy.net>
2021-05-22 20:29:17 +0800

    feat: disable elk for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,5 +6,5 @@ import { IEnvironment } from './environment.types';
 export const environment: IEnvironment = merge({}, comEnv, devEnv, {
   name: 'prod',
   production: true,
-  // elkEndpoint: '',   // set empty to disable elk or new endpoint for production
+  elkEndpoint: '', // set empty to disable elk or new endpoint for production
 });
