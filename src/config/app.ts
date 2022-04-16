64d04dcb7a7d3abc5a95fc68a4ab0f9b05e26350
Bendy Zhang <zb@bndy.net>
2022-04-16 08:38:45 +0800

    style: code format

--- src/config/app.ts
+++ src/config/app.ts
@@ -2,13 +2,16 @@ import { ThemeEntity } from 'src/app/app-types';
 
 export const app = {
   title: 'Angular Starter Project by Bendy',
+
   // TODO: use localization instead
   roles: {
     role_admin: 'Administrator',
     role_doc: 'Document Owner',
   },
+
   // TODO: define your protected page
   protectedUrlPrefixes: [], // ['/admin'];
+
   // The keys MUST be defined in ../styles/_theme.scss file
   themes: [
     {
